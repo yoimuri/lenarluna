4e991878859cd1cdc406d4635f3758b573b9cd97
lnrstory <lnrstory11@gmail.com>
2026-09-17 19:37:17 +0800

    Change 'Events Coverage' to 'Client Showcase'

--- edit-me/7-words-on-the-site.ts
+++ edit-me/7-words-on-the-site.ts
@@ -69,7 +69,7 @@ export const words = {
     "08-corporate-events": "Corporate Events",
     "09-portraits": "Portraits",
     "10-graduation": "Graduation",
-    "11-events-coverage": "Events Coverage",
+    "11-events-coverage": "Client Showcase",
   } as Record<string, string>,
 
   // ---- Section 4: Videos (currently hidden -- see 4-videos.ts) ----
